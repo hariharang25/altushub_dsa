c9794db5bb7bb2aeaafdf53998d66e2d07bf801a
Gonela Hariharan Mudhiraj <123871730+hariharang25@users.noreply.github.com>
2025-07-11 14:48:28 +0530

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,8 +3,8 @@ import { signOut, signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/register", register);
+router.post("/login", login);
 router.get("/signout", signOut);
 
 export default router;
